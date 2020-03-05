dd29759103e7047abfde5ce4dfc8b43ab81267a7
Jan Erik Egge <janegge1990@gmail.com>
2020-03-05 22:11:30 +0100

    start of layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+  CodeOutlined
+} from '@ant-design/icons';
+import { IoLogoJavascript } from "react-icons/io";
+import { MdComputer } from "react-icons/md";
+import { FaUserAstronaut } from "react-icons/fa";
+import "./App.css";
 
-function App() {
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function App() {
+
+  const [collapsed, setCollapsed] = useState(false)
+
+  const onCollapse = collapsed => {
+    setCollapsed(collapsed)
+  };
+
+  const time = new Date()
+
   return (
-    <div className="App">
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" >
+          <CodeOutlined />
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">
+            <span role="img" aria-label="Javascript" className="anticon">
+              <FaUserAstronaut />
+            </span>
+            <span>About me</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <span role="img" aria-label="Javascript" className="anticon">
+              <MdComputer />
+            </span>
+            <span>Projects</span>
+          </Menu.Item>
+          <Menu.Item key="9">
+            <span role="img" aria-label="Javascript" className="anticon">
+              <IoLogoJavascript />
+            </span>
+            <span>Skills</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 128px' }}>
+          <div className="site-layout-background" style={{ margin: '100px 0px 0px 0px', padding: 24, minHeight: '60%', minWidth: 150 }}>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©{time.getFullYear()} Created by Jan Erik</Footer>
+      </Layout>
+    </Layout>
   );
 }
-
-export default App;
